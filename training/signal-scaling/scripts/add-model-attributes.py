4bcbf0b341cd1e498fb6fef0cc0fdb9d6a2bfe8f
Hyeshik Chang <hyeshik@snu.ac.kr>
2018-09-12 11:53:13 +0900

    Add trainer pipeline for DNN-based signal scaler.

---
+++ training/signal-scaling/scripts/add-model-attributes.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2018 Hyeshik Chang
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
+import h5py
+
+with h5py.File('trained-mode.lstm-0.94-0.90/final-model.hdf5') as h5:
+    attrs = h5['model_weights'].attrs
+    attrs['output_transform'] = open('dataarrays/scaling-transform.txt', 'rb').read()
+    attrs['input_defs'] = repr({
+        'dtype': 'float32',
+        'stride': 15,
+        'length': 30000,
+        'min_length': 9000,
+    }).encode('ascii')
+    attrs['model_version'] = b'poreplex-scaler r1 (2018-09-09)'
+
--- training/signal-scaling/scripts/learn-scaling.py
+++ training/signal-scaling/scripts/learn-scaling.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright (c) 2018 Hyeshik Chang
+# Copyright (c) 2018 Institute for Basic Science
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -21,7 +21,7 @@
 # THE SOFTWARE.
 #
 
-from keras.layers import Dense, Dropout, GRU, CuDNNGRU
+from keras.layers import Dense, Dropout, LSTM, CuDNNLSTM
 from keras.models import Sequential
 from keras.callbacks import Callback, EarlyStopping, CSVLogger, ModelCheckpoint
 from keras.utils.training_utils import multi_gpu_model
@@ -36,24 +36,30 @@ import random
 import pickle
 import os
 
-def build_layers(input_shape, output_variables):
-    gru = GRU # CuDNNGRU
+def build_layers(input_shape, output_variables, cudnn=False):
+    if cudnn:
+        lstm_layer = CuDNNLSTM
+        lstm_options = {}
+    else:
+        lstm_layer = LSTM
+        lstm_options = {'recurrent_activation': 'sigmoid'}
 
     model = Sequential()
 
-    model.add(gru(128, return_sequences=True, input_shape=input_shape))
-    model.add(Dropout(0.2))
+    model.add(lstm_layer(128, return_sequences=True, input_shape=input_shape,
+                         **lstm_options))
+    model.add(Dropout(0.35))
 
-    model.add(gru(128, return_sequences=False))
-    model.add(Dropout(0.4))
+    model.add(lstm_layer(128, return_sequences=False, **lstm_options))
+    model.add(Dropout(0.5))
 
     model.add(Dense(output_variables, activation='linear'))
     return model
 
-def create_model(params, input_shape, num_outputs):
+def create_training_model(params, input_shape, num_outputs):
     print('Creating model...')
     with tf.device('/cpu:0'):
-        model = build_layers(input_shape, num_outputs)
+        model = build_layers(input_shape, num_outputs, cudnn=True)
 
     print('Compiling...')
 
@@ -73,10 +79,25 @@ def create_model(params, input_shape, num_outputs):
 
     return model, pmodel
 
+def convert_model_to_noncudnn(input_file, output_file, input_shape, num_outputs,
+                              params):
+    with tf.device('/cpu:0'):
+        cpu_model = build_layers(input_shape, num_outputs, cudnn=False)
+
+        cpu_model.compile(loss='mean_squared_error',
+                          optimizer=params['optimizer'], metrics=['mae'])
+
+    cpu_model.load_weights(input_file)
+    cpu_model.save(output_file)
+    return cpu_model
+
 def train_model(model, pmodel, global_params, training_input, training_output, output_dir):
     callbacks = [
         # Logging
         CSVLogger(output_dir + '/training-log.csv'),
+        # Save best models
+        ModelCheckpoint(output_dir + '/checkpoint-best.hdf5', save_best_only=True,
+                        monitor='val_loss', save_weights_only=True),
         # Early stop at stalled learning
         EarlyStopping(monitor='val_loss',
             min_delta=global_params['earlystopping_min_delta'],
@@ -90,7 +111,7 @@ def train_model(model, pmodel, global_params, training_input, training_output, o
                       validation_split=global_params['validation_split'],
                       verbose=1, callbacks=callbacks)
 
-    model.save(output_dir + '/final-model.hdf5')
+    model.save(output_dir + '/final-cudamodel.hdf5')
 
 def evaluate_model(model, global_params, output_dir):
     testing_input = open_memmap(global_params['testing-x'])
@@ -122,8 +143,8 @@ def main(global_params, output_dir):
     training_input = open_memmap(global_params['training-x'])
     training_output = open_memmap(global_params['training-y'])
 
-    model, pmodel = create_model(global_params, training_input.shape[1:],
-                                 training_output.shape[1])
+    model, pmodel = create_training_model(global_params, training_input.shape[1:],
+                                          training_output.shape[1])
 
     if os.path.isdir(output_dir):
         if any((not f.startswith('.')) for f in os.listdir(output_dir)):
@@ -135,9 +156,20 @@ def main(global_params, output_dir):
 
     train_model(model, pmodel, global_params, training_input, training_output, output_dir)
 
+    print('Adopting the weights to a new model for CPU')
+    cpu_model = \
+        convert_model_to_noncudnn(os.path.join(output_dir, 'final-cudamodel.hdf5'),
+                                  os.path.join(output_dir, 'final-model.hdf5'),
+                                  training_input.shape[1:], training_output.shape[1],
+                                  global_params)
+
     del training_input, training_output
 
+    print('Evaluation of the CUDA model')
     evaluate_model(pmodel, global_params, output_dir)
+
+    print('Evaluation of the CPU model')
+    evaluate_model(cpu_model, global_params, output_dir)
 
 
 if __name__ == '__main__':
@@ -146,7 +178,7 @@ if __name__ == '__main__':
     global_params = {
         'ngpu': 2,
         'optimizer': 'adam',
-        'earlystopping_min_delta': 0.01,
+        'earlystopping_min_delta': 0.001,
         'earlystopping_patience': 10,
         'batchsize_train': 1024,
         'batchsize_test': 1024,
