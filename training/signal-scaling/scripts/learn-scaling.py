00efdeea457b2a2da8408547b35e6c6e52f29a44
Hyeshik Chang <hyeshik@snu.ac.kr>
2019-10-16 22:44:14 +0900

    Update to TF 2.0

--- training/signal-scaling/scripts/learn-scaling.py
+++ training/signal-scaling/scripts/learn-scaling.py
@@ -21,10 +21,6 @@
 # THE SOFTWARE.
 #
 
-from keras.layers import Dense, Dropout, LSTM, CuDNNLSTM
-from keras.models import Sequential
-from keras.callbacks import Callback, EarlyStopping, CSVLogger, ModelCheckpoint
-from keras.utils.training_utils import multi_gpu_model
 from numpy.lib.format import open_memmap
 from scipy.stats import pearsonr
 import shutil
@@ -37,81 +33,83 @@ import pickle
 import os
 
 def build_layers(input_shape, output_variables, cudnn=False):
-    if cudnn:
-        lstm_layer = CuDNNLSTM
-        lstm_options = {}
-    else:
-        lstm_layer = LSTM
-        lstm_options = {'recurrent_activation': 'sigmoid'}
+    def lstm_layer(units, return_sequences):
+        if cudnn:
+            return tf.keras.layers.LSTM(units=units, return_sequences=return_sequences)
+        else:
+            return tf.keras.layers.RNN(tf.keras.layers.LSTMCell(units=units),
+                                       return_sequences=return_sequences)
 
-    model = Sequential()
+    model = tf.keras.models.Sequential()
 
-    model.add(lstm_layer(48, return_sequences=True, input_shape=input_shape,
-                         **lstm_options))
-    model.add(Dropout(0.1))
+    model.add(tf.keras.layers.GaussianNoise(1.5, input_shape=input_shape))
 
-    model.add(lstm_layer(48, return_sequences=False, **lstm_options))
-    model.add(Dropout(0.2))
+    model.add(tf.keras.layers.Bidirectional(lstm_layer(units=48, return_sequences=True)))
+    model.add(tf.keras.layers.Dropout(0.1))
 
-    model.add(Dense(output_variables, activation='linear'))
+    model.add(lstm_layer(units=48, return_sequences=False))
+    model.add(tf.keras.layers.Dropout(0.2))
+
+    model.add(tf.keras.layers.BatchNormalization())
+
+    model.add(tf.keras.layers.Dense(output_variables, activation='linear'))
     return model
 
 def create_training_model(params, input_shape, num_outputs):
-    print('Creating model...')
-    with tf.device('/cpu:0'):
-        model = build_layers(input_shape, num_outputs, cudnn=True)
-
     print('Compiling...')
-
-    if params['ngpu'] > 1:
-        pmodel = multi_gpu_model(model, gpus=params['ngpu'])
-        pmodel.compile(loss='mean_squared_error',
-                       optimizer=params['optimizer'],
-                       metrics=['mae'])
-    else:
-        pmodel = model
-
-    model.compile(loss='mean_squared_error',
-                  optimizer=params['optimizer'],
-                  metrics=['mae'])
+    strategy = tf.distribute.MirroredStrategy()
+    with strategy.scope():
+        model = build_layers(input_shape, num_outputs, cudnn=True)
+        model.compile(loss='mean_squared_error',
+                      optimizer=params['optimizer'],
+                      metrics=['mae'])
 
     print('  - done.')
 
-    return model, pmodel
+    return model
 
-def convert_model_to_noncudnn(input_file, output_file, input_shape, num_outputs,
-                              params):
+def convert_model_to_noncudnn(cudamodel, global_params, training_input,
+                              training_output, output_dir):
+    num_outputs = training_output.shape[1]
+    input_shape = training_input.shape[1:]
+
     with tf.device('/cpu:0'):
         cpu_model = build_layers(input_shape, num_outputs, cudnn=False)
+        cpu_model.set_weights(cudamodel.get_weights())
 
         cpu_model.compile(loss='mean_squared_error',
-                          optimizer=params['optimizer'], metrics=['mae'])
+                          optimizer=global_params['optimizer'],
+                          metrics=['mae'])
 
-    cpu_model.load_weights(input_file)
-    cpu_model.save(output_file)
+        cpu_model.fit(training_input, training_output,
+                      batch_size=global_params['batchsize_train'],
+                      validation_split=global_params['validation_split'],
+                      epochs=1, verbose=1)
+
+    cpu_model.save(os.path.join(output_dir, 'final-model.hdf5'))
     return cpu_model
 
-def train_model(model, pmodel, global_params, training_input, training_output, output_dir):
+def train_model(model, global_params, training_input, training_output, output_dir):
     callbacks = [
         # Logging
-        CSVLogger(output_dir + '/training-log.csv'),
+        tf.keras.callbacks.CSVLogger(os.path.join(output_dir, 'training-log.csv')),
         # Save best models
-        ModelCheckpoint(output_dir + '/checkpoint-best.hdf5', save_best_only=True,
-                        monitor='val_loss', save_weights_only=True),
+        tf.keras.callbacks.ModelCheckpoint(os.path.join(output_dir, 'checkpoint-best.hdf5'),
+                save_best_only=True, monitor='val_loss', save_weights_only=True),
         # Early stop at stalled learning
-        EarlyStopping(monitor='val_loss',
+        tf.keras.callbacks.EarlyStopping(monitor='val_loss',
             min_delta=global_params['earlystopping_min_delta'],
             patience=global_params['earlystopping_patience'],
             verbose=1)
     ]
 
-    hist = pmodel.fit(training_input, training_output,
-                      batch_size=global_params['batchsize_train'],
-                      epochs=global_params['epochs'],
-                      validation_split=global_params['validation_split'],
-                      verbose=1, callbacks=callbacks)
+    hist = model.fit(training_input, training_output,
+                     batch_size=global_params['batchsize_train'],
+                     epochs=global_params['epochs'],
+                     validation_split=global_params['validation_split'],
+                     verbose=1, callbacks=callbacks)
 
-    model.save(output_dir + '/final-cudamodel.hdf5')
+    model.save(os.path.join(output_dir, 'final-cudamodel.hdf5'))
 
 def evaluate_model(model, global_params, output_dir):
     testing_input = open_memmap(global_params['testing-x'])
@@ -119,7 +117,7 @@ def evaluate_model(model, global_params, output_dir):
 
     xfrm_params = eval(open(global_params['transform-y']).read())
 
-    predmtx = model.predict(testing_input, global_params['batchsize_test'], verbose=1)
+    predmtx = model.predict(testing_input, global_params['batchsize_test'])
     expected_mtx = np.array([
         (testing_output[:, 0] * xfrm_params['scale_std']) + xfrm_params['scale_mean'],
         (testing_output[:, 1] * xfrm_params['shift_std']) + xfrm_params['shift_mean']
@@ -143,8 +141,8 @@ def main(global_params, output_dir):
     training_input = open_memmap(global_params['training-x'])
     training_output = open_memmap(global_params['training-y'])
 
-    model, pmodel = create_training_model(global_params, training_input.shape[1:],
-                                          training_output.shape[1])
+    model = create_training_model(global_params, training_input.shape[1:],
+                                  training_output.shape[1])
 
     if os.path.isdir(output_dir):
         if any((not f.startswith('.')) for f in os.listdir(output_dir)):
@@ -154,19 +152,17 @@ def main(global_params, output_dir):
     else:
         os.makedirs(output_dir)
 
-    train_model(model, pmodel, global_params, training_input, training_output, output_dir)
+    train_model(model, global_params, training_input, training_output, output_dir)
 
     print('Adopting the weights to a new model for CPU')
     cpu_model = \
-        convert_model_to_noncudnn(os.path.join(output_dir, 'final-cudamodel.hdf5'),
-                                  os.path.join(output_dir, 'final-model.hdf5'),
-                                  training_input.shape[1:], training_output.shape[1],
-                                  global_params)
+        convert_model_to_noncudnn(model, global_params, training_input,
+                                  training_output, output_dir)
 
     del training_input, training_output
 
     print('Evaluation of the CUDA model')
-    evaluate_model(pmodel, global_params, output_dir)
+    evaluate_model(model, global_params, output_dir)
 
     print('Evaluation of the CPU model')
     evaluate_model(cpu_model, global_params, output_dir)
@@ -176,13 +172,12 @@ if __name__ == '__main__':
     random.seed(922)
 
     global_params = {
-        'ngpu': 2,
         'optimizer': 'adam',
-        'earlystopping_min_delta': 0.0005,
+        'earlystopping_min_delta': 0.001,
         'earlystopping_patience': 30,
-        'batchsize_train': 3072,
-        'batchsize_test': 2048,
-        'epochs': 300,
+        'batchsize_train': 1024,
+        'batchsize_test': 1024,
+        'epochs': 500,
         'validation_split': 0.25,
         'transform-y': 'dataarrays/scaling-transform.txt',
         'training-x': 'dataarrays/signals-training.npy',
