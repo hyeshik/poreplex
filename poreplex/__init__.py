d632bc78df7cb1cf57cdc50a7405494503b64231
Hyeshik Chang <hyeshik@snu.ac.kr>
2019-10-18 10:21:46 +0900

    Bump version to 0.5

--- poreplex/__init__.py
+++ poreplex/__init__.py
@@ -27,7 +27,7 @@ __all__ = [
     'OUTPUT_NAME_UNDETERMINED', 'OUTPUT_NAME_BARCODING_OFF',
 ]
 
-__version__ = '0.4.1'
+__version__ = '0.5'
 
 OUTPUT_NAME_PASSED = 'pass'
 OUTPUT_NAME_FAILED = 'fail'
